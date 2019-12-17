171a0ec79ffb53975d92b327f1c0374338bba3c8
Tier5abhisek <abhisek.bose@tier5.in>
2019-12-17 12:07:15 +0530

    Version change for sentry

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 import * as Sentry from '@sentry/browser';
 
-Sentry.init({dsn: "https://c4de7c3e95474ecb9ee1c1ce651ffa25@sentry.io/1853932", release: "ebAWS@2.0.0"});
+Sentry.init({dsn: "https://c4de7c3e95474ecb9ee1c1ce651ffa25@sentry.io/1853932", release: "ebAWS@3.0.0"});
 
 ReactDOM.render(
   <App />,
