165a97fb5b2e75ad8e9c63f947ab492cbab1d661
Tier5abhisek <abhisek.bose@tier5.in>
2019-12-17 12:14:40 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Tier5, This is Abhisek Bose Automation setup</h2>
+          <h2>Welcome to Tier5, This is Abhisek Bose Automation setup.</h2>
         </div>
         <p className="App-intro">
          It's Working..
